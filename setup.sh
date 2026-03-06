cc2fa86c03df4b0c6a650526566f4a8995197fb2
katohirosato <hirosato654@gmail.com>
2026-03-06 08:34:31 +0000

    first commit

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+REPONAME=${1:-aws-app}
+mkdir -p ${REPONAME}
+cd ${REPONAME}
+curl -L -o .devcontainer.json https://raw.githubusercontent.com/katohirosato/aws-dev/main/.devcontainer.json
+# > Dev Containers: Rebuild and Reopen in Container as .devcontainer.json
+git init
+git branch -M main
+git remote add origin https://github.com/<repo-owner>/${REPONAME}.git
+git config user.name "<Your Name>"
+git config user.email "<your.email@example.com>"
+git add .
+git commit -m "first commit"
+git push -u origin main
+# > Dev Containers: Reopen Folder in WSL
+# > Dev Containers: Rebuild and Reopen in Container as .devcontainer/devcontainer.json
+aws configure sso
+aws sso login
